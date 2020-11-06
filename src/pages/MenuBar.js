87f20b79c43dbf1f7afb0675f60d407a1a974174
José Eduardo de S. Oliveira <eduardo.jeso@gmail.com>
2020-11-06 16:00:16 -0300

    Update MenuBar.js

--- src/pages/MenuBar.js
+++ src/pages/MenuBar.js
@@ -1,10 +1,4 @@
-import {
-  Card,
-  InputBase,
-  makeStyles,
-  Typography,
-  TextField,
-} from '@material-ui/core';
+import { Card, makeStyles, Typography, TextField } from '@material-ui/core';
 import React from 'react';
 
 const useStyles = makeStyles(() => ({
