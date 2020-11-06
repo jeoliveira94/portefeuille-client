8e316969ce15be3d115b77168c365e06204e2f8f
José Eduardo de S. Oliveira <eduardo.jeso@gmail.com>
2020-11-06 16:41:52 -0300

    Update MainRouter.js
    adicionada rota para cadastro

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,14 +1,16 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Cadastro from './pages/cadastro/Cadastro';
 
-import Catalogo from "./pages/catalogo/Catalogo";
-import MenuBar from "./pages/MenuBar";
+import Catalogo from './pages/catalogo/Catalogo';
+import MenuBar from './pages/MenuBar';
 
 export default function MainRouter() {
   return (
-    <div style={{backgroundColor: "#eee"}}>
-      <MenuBar/>
+    <div style={{ backgroundColor: '#eee' }}>
+      <MenuBar />
       <Switch>
+        <Route path="/cadastro" component={Cadastro} />
         <Route path="/" component={Catalogo} />
       </Switch>
     </div>
