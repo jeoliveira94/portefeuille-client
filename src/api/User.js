639f3623e1a6cacd8ef805ceeae9dcb800a252b7
Weverton Trindade <weverton2trindade@gmail.com>
2020-11-25 10:36:53 -0300

    Servico Aluno atualizado

--- src/api/User.js
+++ src/api/User.js
@@ -2,43 +2,29 @@ import Axios from "axios";
 
 const apiUrl = "localhost:8080/api/v1/alunos";
 
-async function create(user) {
-  let response;
-  try {
-    await Axios.post(apiUrl, user, {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-    })
-      .then((res) => {
-        response = res.data;
-      })
-      .catch((error) => {
-        response = error.response.data;
-      });
-    return response;
-  } catch (error) {
-    console.log(error);
-  }
+async function create(aluno) {
+  const options = {
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+  };
+  const response = await Axios.post(apiUrl, aluno, options);
+  const resposta = response.data;
+  return resposta;
 }
 
 async function list() {
-  let response;
-  try {
-    await Axios.get(apiUrl, {
-      headers: { timeout: 3000 },
-    })
-      .then((res) => {
-        response = res.data;
-      })
-      .catch((error) => {
-        response = error.response.data;
-      });
-    return response;
-  } catch (error) {
-    console.log(error);
-  }
+  const options = {
+    url: apiUrl,
+    method: "GET",
+    headers: {
+      "Access-Control-Allow-Credentials": true,
+    },
+  };
+  const response = await Axios(options);
+  const alunos = response.data;
+  console.log(alunos);
+  return alunos;
 }
 
 export { create, list };
