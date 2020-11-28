291c08f989f19fd6888f8f711a8b294a74357029
Weverton Trindade <weverton2trindade@gmail.com>
2020-11-28 14:42:13 -0300

    Dando um trato na barra de navegação

--- src/pages/MenuBar.js
+++ src/pages/MenuBar.js
@@ -1,11 +1,43 @@
-import { Card, makeStyles, Typography, TextField } from "@material-ui/core";
+import {
+  Card,
+  makeStyles,
+  Typography,
+  AppBar,
+  Button,
+  Toolbar,
+  TextField,
+  withStyles,
+} from "@material-ui/core";
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const useStyles = makeStyles(() => ({
+const useStyles = (theme) => ({
   root: {
-    display: "flex",
+    background: "rgba(44, 45, 51)",
+    color: "#FFF",
+  },
+  title: {
+    color: "#FFF",
+    padding: theme.spacing(2),
+    textDecoration: "none",
+    transition: "0.3s",
+    "&:visited, &:hover, &:active": {
+      textDecoration: "none",
+    },
+    "&:hover": {
+      background: "rgb(39, 91, 102)",
+    },
+  },
+  content: {
+    marginLeft: theme.spacing(5),
+    marginRight: theme.spacing(5),
     justifyContent: "space-between",
+  },
+  links: {
+    textDecoration: "none",
+    "&:visited, &:hover, &:active": {
+      textDecoration: "none",
+    },
   },
   search: {
     borderRadius: "4px",
@@ -17,31 +49,49 @@ const useStyles = makeStyles(() => ({
     marginBottom: 10,
     paddingBottom: "10px",
   },
-  title: {
-    fontSize: "2.5rem",
-    paddingLeft: "10%",
-    paddingTop: "10px",
-    marginTop: 10,
-    color: "#000",
-  },
-}));
+});
 
-export default function MenuBar() {
-  const classes = useStyles();
-
+function MenuBar(props) {
+  const { classes } = props;
   return (
-    <Card className={classes.root}>
-      <NavLink to="/" style={{ textDecoration: "none" }}>
-        <Typography variant="h1" className={classes.title}>
-          Portefeuille
-        </Typography>
-      </NavLink>
-      <TextField
-        label="Buscar"
-        variant="outlined"
-        inputProps={{ "arial-label": "search" }}
-        className={classes.search}
-      />
-    </Card>
+    <AppBar className={classes.root} position="static">
+      <Toolbar className={classes.content}>
+        <Link to="/" className={classes.title}>
+          <Typography variant="h6">Portefeuille</Typography>
+        </Link>
+        <div className={classes.links}>
+          {
+            <span>
+              <Link to="/cadastro" className={classes.links}>
+                <Button className={classes.title}>Cadastrar</Button>
+              </Link>
+              <Link to="/acesso" className={classes.links}>
+                <Button className={classes.title}>Acessar</Button>
+              </Link>
+            </span>
+          }
+          {/*auth.isAuthenticated() && (
+            <span>
+              <Link
+                to={`/user/${auth.isAuthenticated().user._id}`}
+                className={classes.links}
+              >
+                <Button className={classes.title}>Meu Perfil</Button>
+              </Link>
+              <Button
+                className={classes.title}
+                onClick={() => {
+                  auth.clearJWT(() => history.push("/"));
+                }}
+              >
+                Sair
+              </Button>
+            </span>
+              )*/}
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 }
+
+export default withStyles(useStyles)(MenuBar);
