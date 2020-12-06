51bc703e2d0b409e6af5ced7c691f1b175b554d5
Weverton Trindade <weverton2trindade@gmail.com>
2020-12-06 17:27:34 -0300

    Criado API dos projetos

---
+++ src/api/Projetos.js
@@ -0,0 +1,43 @@
+import Axios from "axios";
+
+const apiUrl = "http://localhost:8080/api/v1/projetos/";
+
+async function create(projeto) {
+  const options = {
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+  };
+  const response = await Axios.post(apiUrl, projeto, options);
+  const resposta = response.data;
+  return resposta;
+}
+
+async function list() {
+  const options = {
+    headers: {
+      "Access-Control-Allow-Credentials": true,
+    },
+  };
+
+  const response = await Axios.get(apiUrl, options);
+  const projetos = response.data;
+
+  return projetos;
+}
+
+async function listByAluno(matricula) {
+  const url = apiUrl + "aluno/" + matricula;
+  const options = {
+    headers: {
+      "Access-Control-Allow-Credentials": true,
+    },
+  };
+
+  const response = await Axios.get(url, options);
+
+  const projeto = response.data;
+  return projeto;
+}
+
+export { create, listByAluno };
